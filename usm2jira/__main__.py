59fc6dc2cafe5cac397222b633a9567cee86e018
Rana Awais <rana.awais@ebryx.com>
2019-03-26 15:50:40 +0500

    reads configuration from json file.

--- usm2jira/__main__.py
+++ usm2jira/__main__.py
@@ -1,3 +1,4 @@
+import os
 import json
 import time
 import logging
@@ -13,26 +14,65 @@ handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
 logger.addHandler(handler)
 
 
-USM_API_URL = str()
-CLIENT_ID = str()
-CLIENT_SECRET = str()
+def read_config():
+
+    if not os.environ.get('CONFIG_FILE'):
+        logger.info('No CONFIG_FILE environment variable is defined.')
+        exit(1)
+
+    if not os.path.isfile(os.environ.get('CONFIG_FILE')):
+        logger.info('No file exists on CONFIG_FILE path defined.')
+        exit(1)
+
+    try:
+        config = json.load(open(os.environ.get('CONFIG_FILE'), 'r'))
+        return config
+    except json.JSONDecodeError as exc:
+        logger.info(exc)
+        exit(1)
 
 
-def get_auth_token():
+def validate_config(config):
 
-    url = urljoin(USM_API_URL, 'oauth/token')
+    if not config.get('usm'):
+        logger.info('No `usm` field defined in config.')
+        exit(1)
+
+    if not config.get('jira'):
+        logger.info('No `jira` field defined in config.')
+        exit(1)
+
+    usm = config['usm']
+    if not(usm.get('api_url') and usm.get(
+            'client_id') and usm.get('client_secret')):
+        logger.info('`api_url`, `client_id` and `client_secret` are '
+                    'required for `usm` field.')
+        exit(1)
+
+    jira = config['jira']
+    if not(jira.get('api_url') and jira.get(
+            'username') and jira.get('api_token')):
+        logger.info('`api_url`, `username` and `api_token` are required '
+                    'for `jira` field.')
+        exit(1)
+
+
+def get_auth_token(config):
+
+    usm = config['usm']
+    url = urljoin(usm.get('api_url'), 'oauth/token')
     logger.info('Making POST request: %s', url)
     res = requests.post(url, data={'grant_type': 'client_credentials'},
-                        auth=(CLIENT_ID, CLIENT_SECRET))
+                        auth=(usm.get('client_id'), usm.get('client_secret')))
 
     if res.status_code < 300:
-        return res.json().get('access_token')
+        return usm.get('api_url'), res.json().get('access_token')
 
     logger.info('Unexpected response returned: %s', res)
-    return None
+    return None, None
 
 
-def get_usm_alarms(token):
+def get_usm_alarms(api_url, token):
 
     c_time = int(str(time.time()).split('.')[0])
     timestamp = str(c_time - (1 * 60))
@@ -40,7 +80,7 @@ def get_usm_alarms(token):
     params = ['page=1', 'size=2', 'sort=timestamp_occured,desc',
               'timestamp_occured_gte=' + timestamp]
 
-    url = urljoin(USM_API_URL, 'alarms?%s' % ('&'.join(params)))
+    url = urljoin(api_url, 'alarms?%s' % ('&'.join(params)))
     logger.info('Making GET request: %s', url)
     res = requests.get(url, headers={'Authorization': 'Bearer ' + token})
     if res.status_code < 300:
@@ -51,6 +91,8 @@ def get_usm_alarms(token):
 
 
 if __name__ == "__main__":
-    token = get_auth_token()
-    alarms = get_usm_alarms(token)
+
+    config = read_config()
+    api_url, token = get_auth_token(config)
+    alarms = get_usm_alarms(api_url, token)
     print(json.dumps(alarms, indent=2))
