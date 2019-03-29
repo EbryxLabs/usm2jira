e3bf9b1aee24f9af753419fecac971332cc5f632
Rana Awais <rana.awais@ebryx.com>
2019-03-29 21:12:44 +0500

    renamed file.

--- usm2jira/__init__.py
+++ usm2jira/__init__.py
@@ -1,4 +1,4 @@
-from .__main__ import (
+from .__script__ import (
     read_config, get_auth_token, get_usm_alarms,
     get_jira_issues, get_jira_projects, get_jira_issue_types,
     filter_alarms, filter_duplicate_tickets,
